28e0fc6147cdf40599e10b7997739d0ba162aac7
poohsocool <109661634+poohsocool@users.noreply.github.com>
2022-10-16 16:36:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image 
+import matplotlib.pyplot as plt
 from itertools import product
 import warnings
 from binance.client import Client
